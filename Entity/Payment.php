5e88a695466a9fba1405c433cc7f9bfd8de64f2a
Maci <maci1011@facebook.com>
2019-11-12 23:30:55 +0100

    general edits

---
+++ Entity/Payment.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maci\OrderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Payum\Core\Model\Payment as BasePayment;
+
+class Payment extends BasePayment
+{
+}
---
+++ Entity/PaymentToken.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Maci\OrderBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Payum\Core\Model\Token;
+
+class PaymentToken extends Token
+{
+}
