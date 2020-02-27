2dfd75686df4f88ec8af6f5a33fbcfbdf568d723
Maci <maci1011@facebook.com>
2020-02-27 02:52:49 +0100

    general edits

--- Form/Type/CartAddProductItemType.php
+++ Form/Type/CartAddProductItemType.php
@@ -38,22 +38,26 @@ class CartAddProductItemType extends AbstractType
 	{
 		$this->product = $options['product'];
 
-		$builder
-			->add('product', HiddenType::class, array(
+		$builder->add('product', HiddenType::class, array(
 				'data' => ($this->product ? $this->product->getId() : null)
-			))
-			->add('quantity', IntegerType::class, array(
+		));
+		if ($this->product && $this->product->getLimited() && 1 < $this->product->getQuantity()) {
+			$builder->add('quantity', IntegerType::class, array(
 				'data' => 1,
 				'label' => ($this->translator->getLabel('cart_add_product_item.label', 'Select Quantity')),
-                'attr' => array_merge(array('class' => 'edit-quantity-field', 'min' => 0),(
-                	($this->product and $this->product->getLimited()) ? array('max' => $this->product->getQuantity()) : array()
-                ))
-            ))
-			->add('add_to_cart', SubmitType::class, array(
-				'label' => ($this->translator->getLabel('product.add_to_cart', 'Add To Cart')),
-                'attr' => array('class' => 'btn-primary btn')
-            ))
-		;
+	            'attr' => array_merge(array('class' => 'edit-quantity-field', 'min' => 0),(
+	            	($this->product && $this->product->getLimited()) ? array('max' => $this->product->getQuantity()) : array()
+	            ))
+	        ));
+		} else {
+			$builder->add('quantity', HiddenType::class, array(
+				'data' => 1
+            ));
+		}
+		$builder->add('add_to_cart', SubmitType::class, array(
+			'label' => ($this->translator->getLabel('product.add_to_cart', 'Add To Cart')),
+            'attr' => array('class' => 'btn-primary btn')
+        ));
 	}
 
 	public function getName()
